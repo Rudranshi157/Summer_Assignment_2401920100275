a6760147ec86500ea31c8b50d042e6b0ded24da9
Rudranshi Mittal <mittal.rudranshi@gmail.com>
2026-06-03 23:51:34 +0530

    Day 3

---
+++ Week_1/Day_3.java
@@ -0,0 +1,71 @@
+public class Day_3{
+
+    // Question 1
+    // Move Zeroes
+    // https://leetcode.com/problems/move-zeroes/
+    // Time Complexity = O(n)     
+    // Space Complexiy = O(1) 
+    public void moveZeroes(int[] nums) {
+        int n=nums.length;
+        int k=0, j=0;
+        while(k<n ){
+            if(nums[k] != 0){
+                int temp = nums[k];
+                nums[k] = nums[j];
+                nums[j] = temp;
+                j++;
+            }
+            k++;
+        }
+        while(j<n){
+            nums[j++]=0;
+        }
+    }
+
+    // Question 2
+    // Squares of a Sorted Array
+    // https://leetcode.com/problems/squares-of-a-sorted-array/
+    // Time Complexity = O(n)     
+    // Space Complexiy = O(n) 
+    public int[] sortedSquares(int[] nums) {
+        int res[] = new int[nums.length];
+        int left=0 , right=nums.length-1;
+        for(int i=nums.length-1 ; i>=0 ; i--){
+            if(Math.abs(nums[right]) > Math.abs(nums[left])){
+                res[i] = nums[right]*nums[right];
+                right--;
+            }else{
+                res[i] = nums[left] * nums[left];
+                left++;
+            }
+        }
+        return res;
+    }
+
+
+    // Question 3
+    // Container With Most Water
+    // https://leetcode.com/problems/container-with-most-water/
+    // Time Complexity = O(n)     
+    // Space Complexiy = O(1) 
+    public int maxArea(int[] height) {
+        int maxw=Integer.MIN_VALUE;
+        int ls=0 ,rs=height.length-1;
+        while(ls<rs){
+            int w=rs-ls;
+            int h=Math.min(height[rs],height[ls]);
+            int water=w*h;
+            maxw=Math.max(water,maxw);
+            if(height[rs]>height[ls]){
+                ls++;
+            }else{
+                rs--;
+            }
+        }
+        return maxw;
+    }
+
+    public static void main(String[] args) {
+        
+    }
+}
